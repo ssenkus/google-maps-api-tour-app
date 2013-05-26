1845fb111cc1872e47f4dbacf7bc3afd0a1c91ab
Steven Senkus <steven.senkus@yahoo.com>
2013-05-26 13:22:50 -0700

    D.R.Y.

--- scripts/gmap-ec-tour.js
+++ scripts/gmap-ec-tour.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function(){
 
-		// Load map
+		// -- Map --
+		// -- -- build Map --
 		var coords = new google.maps.LatLng('45.09', '-122.77');
 		var mapOptions = {
 			zoom: 7,
@@ -13,91 +14,69 @@ $(document).ready(function(){
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		
+		
 		var markersActive = [];
+		var tourStopsCoordinates = [];
+		var tourPath;
 		
-		var tourPath = new google.maps.Polyline({
-		});
+		// -- -- Map functions --
 		
 		
-		// Click functions		
-		//		click on map -> place marker
-		function placeMarker(location) {
-			var clickMarker = new google.maps.Marker({
-				position: location,
-				map: map
-			});
-			markersActive.push(clickMarker);
-			tourStopsCoordinates.push(location);
+		// Marker Data functions
+		function addToLocationList (geocodeAddress) {
+				$("#location-list").append('<li>'+ geocodeAddress + " </li>");
 		}
 		
-		//		Geolocate button -> place marker at IP address geolocation
+		// -- Click functions --
+		
+		function addMarker(latitude, longitude) {
+			var markerCoords = new google.maps.LatLng(latitude, longitude);
+			// get title from 
+			$.ajax({
+				type:		"GET",
+				url: 		"http://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude+"&sensor=true",
+				dataType:	"json",
+				success:    function(data) {
+								
+								var currentAddress = data.results[1]['formatted_address'];
+								
+								var marker = new google.maps.Marker({
+									position: markerCoords,
+									map: map,
+									title: currentAddress,
+									animation: google.maps.Animation.DROP
+								});
+								addToLocationList(currentAddress);
+								markersActive.push(marker);
+								tourStopsCoordinates.push(markerCoords);
+							},
+				error: 		function(xhr, ajaxOptions, thrownError) {  
+								alert("xhr status: " + xhr.statusText +"\nError thrown: " + thrownError);    
+							}
+			});
+		}
+		
+		// -- -- 'Geolocate' button -> place marker at IP address geolocation
 		$('#geolocate').click(function() {
 			if (navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition(function(position){
 					var latitude = position.coords.latitude;
 					var longitude = position.coords.longitude;
-					var coords = new google.maps.LatLng(latitude, longitude);
-					
-					$.ajax({
-						type:		"GET",
-						url: 		"http://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude+"&sensor=true",
-						dataType:	"json",
-						success:    function(data) {
-										
-										var currentAddress = data.results[1]['formatted_address'];
-										$("#location-list").append('<li>'+ currentAddress + " </li>");
-										var marker = new google.maps.Marker({
-											position: coords,
-											map: map,
-											title: "Placeholder for address returned by Google AJAX call!"
-										});
-										markersActive.push(marker);
-										tourStopsCoordinates.push(coords);
-									},
-						error: 		function(xhr, ajaxOptions, thrownError) {  
-										alert("xhr status: " + xhr.statusText +"\nError thrown: " + thrownError);    
-									}
-					});
+					addMarker(latitude, longitude);
 				});
 			}
 		});
 		
 		
-		//		Add Tour Point with Coordinates -> 
+		// -- -- 'Add Tour Point' with input field Coordinates -> 
 		$('#addTourPoint').click(function(){
-				var markerLat = $('#addTPLat').val();;
-				var markerLng = $('#addTPLng').val();
-				var myLatLng = new google.maps.LatLng(markerLat, markerLng);
-						var marker = new google.maps.Marker({
-						position: myLatLng,
-						map: map,
-				//		icon: ecIcon,
-						animation: google.maps.Animation.DROP,
-						title: 'Added point' 
-				});
-				
-				$.ajax({
-					type:		"GET",
-					url: 		"http://maps.googleapis.com/maps/api/geocode/json?latlng="+markerLat+","+markerLng+"&sensor=false",
-					dataType:	"json",
-					async: false,
-					success:    function(data) {
-
-									var currentAddress = data.results[1]['formatted_address'];
-									$("#location-list").append('<li>'+ currentAddress + " </li>");
-									marker.title = currentAddress;
-								},
-					error: 		function(xhr, ajaxOptions, thrownError) {  
-									alert("xhr status: " + xhr.statusText +"\nError thrown: " + thrownError);    
-								}
-				});			
-			
-				markersActive.push(marker);
-				tourStopsCoordinates.push(myLatLng);
-				
-				
+				// get input field values
+				var inputLat = $('#addTPLat').val();
+				var inputLng = $('#addTPLng').val();
+				addMarker(inputLat, inputLng);
 		});
-			var tourStopsCoordinates = [];		
+		
 		
 		
 		//		Add 4 dummy markers
@@ -123,52 +102,25 @@ $(document).ready(function(){
 					}
 			};
 			
-			var storedMarkersCount = Object.keys(storedMarkers).length
-
-			for (var x = 0; x < storedMarkersCount; x++) {
-			
-				var markerLat = storedMarkers[x].lat;
-				var markerLng = storedMarkers[x].lng;
-				var myLatLng = new google.maps.LatLng(markerLat, markerLng);
-
-				tourStopsCoordinates.push(myLatLng);
-
-				var ecIcon = "images/ecIcon.png";
-				var marker = new google.maps.Marker({
-						position: myLatLng,
-						map: map,
-						icon: ecIcon,
-						animation: google.maps.Animation.DROP,
-						title: 'title' + x
-				});
-				markersActive.push(marker);
-
-				$.ajax({
-					type:		"GET",
-					url: 		"http://maps.googleapis.com/maps/api/geocode/json?latlng="+markerLat+","+markerLng+"&sensor=false",
-					dataType:	"json",
-					ajaxI: x, 
-					async: false,
-					success:    function(data) {
-									var i = this.ajaxI
-									var ecIcon = "images/ecIcon.png";
-									var currentAddress = data.results[1]['formatted_address'];
-									$("#location-list").append('<li><img src="' + ecIcon + '" />'+ currentAddress + " </li>");
-									markersActive[i].title = currentAddress;
-									
-								},
-					error: 		function(xhr, ajaxOptions, thrownError) {  
-									alert("xhr status: " + xhr.statusText +"\nError thrown: " + thrownError);    
-								}
-				});			
-			
+			//var storedMarkersCount = Object.keys(storedMarkers).length
+			for (var location in storedMarkers) {
+				
+				var markerLat = storedMarkers[location].lat;
+				var markerLng = storedMarkers[location].lng;
+				
+				addMarker(markerLat, markerLng);
 			}
+			/*for (var i = 0; i < storedMarkersCount; i++) {
+				var markerLat = storedMarkers[i].lat;
+				var markerLng = storedMarkers[i].lng;
+				addMarker(markerLat, markerLng);
+			}*/
 			
 		});
 
 		//		Draw tour path button
 		$('#drawTourPath').click(function () {
-			console.log(tourStopsCoordinates);
+
 			tourPath = new google.maps.Polyline({
 				path: tourStopsCoordinates,
 				strokeColor: "#F00",
@@ -182,7 +134,10 @@ $(document).ready(function(){
 		
 		
 		google.maps.event.addListener(map, 'click', function(event) {
-			placeMarker(event.latLng);
+		//		click on map -> place marker
+			var clickLat = event.latLng['jb'];
+			var clickLng = event.latLng['kb'];
+			addMarker(clickLat, clickLng);
 		});
 
 		//		clear markers and routes
@@ -192,22 +147,9 @@ $(document).ready(function(){
 			console.log(tourPath);
 			tourPath.setMap(null);
 			markersActive = [];
+
 		});
 	
-	/* Geolocation code
-		if(navigator.geolocation) {
-			alert("Great..Geolocation API is supported.");
-		} else {
-			alert("Geolocation API is not supported in your browser.");
-		}
-		navigator.geolocation.getCurrentPosition(function(position) {
-			console.log(position);
-			var latitude = position.coords.latitude;
-			var longitude = position.coords.longitude;
-			alert('latitude:' + latitude + '\n' + 'longitude:' + longitude);
-		});
-		
-*/
 
 /*
   var weatherLayer = new google.maps.weather.WeatherLayer({
